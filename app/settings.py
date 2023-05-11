ae6132b5a06e5a387a6b9cfed3199bd4e9db44be
Taylor Witherell <trwitherell@gmail.com>
2023-05-11 00:01:50 -0400

    Working on getting database properly integrated

--- app/settings.py
+++ app/settings.py
@@ -79,11 +79,26 @@ WSGI_APPLICATION = "app.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PWD"),
+        "NAME": env("DB_NAME"),
+        "HOST": env("DB_HOST"),
+        "PORT": int(env("DB_PORT").strip('"')),
+        "CONN_MAX_AGE": 0,
+        "TEST": {
+            "DEPENDENCIES": [],
+        },
+    },
 }
 
 
--- test_files/test_movies.py
+++ test_files/test_movies.py
@@ -21,13 +21,28 @@ def list_movies():
     return response
 
 
+def create_movie(data):
+    endpoint = f"{API_ROOT}/movies"
+    response = requests.post(endpoint, data=data)
+    return response
+
+
 if __name__ == "__main__":
     from pprint import PrettyPrinter
 
     pp = PrettyPrinter(depth=10)
 
+    MOVIE_CREATE_DATA = {
+        "title": "Endgame",
+        "genre": "Fiction",
+        "release_date": "1999-05-04",
+    }
+
     # health = get_health()
     # pp.pprint(health.json())
 
     movie = list_movies()
+    pp.pprint(movie.json())
+
+    movie = create_movie(MOVIE_CREATE_DATA)
     pp.pprint(movie.json())

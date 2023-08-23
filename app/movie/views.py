2b392e4dff066212a2d140b38ecc259870de7294
Taylor Witherell <trwitherell@gmail.com>
2023-08-23 19:20:40 -0400

    enable circleci

--- app/movie/views.py
+++ app/movie/views.py
@@ -1,7 +1,7 @@
 import logging
 
 from django.shortcuts import render
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, mixins
 
 from app.movie.models import Movie
 from app.movie.serializers import MovieSerializer
@@ -9,7 +9,7 @@ from app.movie.serializers import MovieSerializer
 logger = logging.getLogger("main")
 
 # Create your views here.
-class MovieListCreateAPIView(ListCreateAPIView):
+class MovieListCreateAPIView(mixins.DestroyModelMixin, ListCreateAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
@@ -20,3 +20,6 @@ class MovieListCreateAPIView(ListCreateAPIView):
     def create(self, request, *args, **kwargs):
         logger.debug("creating movie")
         return super().create(request, *args, **kwargs)
+    
+    def delete(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
